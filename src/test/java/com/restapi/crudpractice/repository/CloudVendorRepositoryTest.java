3e024d47ff3599bab6ca7728eac45dd2269c70e3
Tejasri Gamidi <tejusmiley999@gmail.com>
2024-02-08 15:53:03 +0530

    commit

---
+++ src/test/java/com/restapi/crudpractice/repository/CloudVendorRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.restapi.crudpractice.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class CloudVendorRepositoryTest {
+
+	
+}
